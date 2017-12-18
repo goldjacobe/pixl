1a6c3273ab1d5aaf2c4d34fb073747b138adc711
Jacob Gold <goldjacobe@gmail.com>
2017-12-18 00:29:19 -0500

    write function in c

--- stdlib.c
+++ stdlib.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_ASSERT(x)
 #include "stb_image.h"
@@ -18,4 +19,43 @@ unsigned long* read_img(char* filename) {
     }
     stbi_image_free(data);
     return img;
+}
+
+int write_img(unsigned long* img, char* filename, char* extension) {
+    int h = (int)(*img);
+    int w = (int)(*(img + 1));
+    unsigned long *data_start = img + 2;
+    unsigned char *data = malloc(sizeof(char) * 4 * w * h); //gonna free this time!
+    for (int i = 0; i < h * w * 4; i++) {
+        unsigned long val = *(data_start + i);
+        if ((long)val < 0l)
+            val = 0ul;
+        if (val > 255ul)
+            val = 255ul;
+        *(data + i) = (unsigned char)val;
+    }
+    int out;
+    char* name = malloc(strlen(filename) + 5);
+    strcpy(name, filename);
+    strcat(name, ".");
+    strcat(name, extension);
+    if (!strcmp(extension, "png")) {
+        out = stbi_write_png(name, w, h, 4, data, 0);
+    } else if (!strcmp(extension, "bmp")) {
+        out = stbi_write_bmp(name, w, h, 4, data);
+    } else if (!strcmp(extension, "tga")) {
+        out = stbi_write_tga(name, w, h, 4, data);
+    } else if (!strcmp(extension, "jpg")) {
+        out = stbi_write_jpg(name, w, h, 4, data, 100);
+    } else {
+        printf("hi\n");
+        return 1;
+    }
+    printf("%d\n", out);
+    free(name);
+    free(data);
+    if (out) {
+        return 0;
+    }
+    return 1;
 }
